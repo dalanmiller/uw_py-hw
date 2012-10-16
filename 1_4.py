3ba1bb86c79f0fe2d6b8e2c7beeb4ffeb780e1f7
Andreas Stassivik <gitstassi@stassi.net>
2012-10-15 22:38:10 -0700

    No more subdirectories, finished ch1

---
+++ 1_4.py
@@ -0,0 +1,7 @@
+#If you run a 10 kilometer race in 43 minutes 30 seconds, what is your average time per mile? What is your average speed in miles per hour? (Hint: there are 1.61 kilometers in a mile).
+
+minutes_per_mile = (43.5 * 1.61) / 10 #minutes per mile
+miles_per_hour = ((10 * 60) / (43.5 * 1.61 )) #miles per hour
+
+print minutes_per_mile
+print miles_per_hour
