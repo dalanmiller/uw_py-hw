658e84281e11146f5951980dddb3e750491bf0a4
Andreas Stassivik <gitstassi@stassi.net>
2012-11-10 15:46:07 -0800

    9_3 is a success!

--- week_5/9_3.py
+++ week_5/9_3.py
@@ -1,31 +1,34 @@
 fin = open('words.txt')
 
 def avoids(avoided):
-	print "Success"
-
-def are_letters(word):
-	print "Are letters!"
-	return True
-
-def are_unique(word):
-	print "Are unique!"
-	return True
+	for line in fin:
+		passes_filter = True
+		word = line.strip()
+		for letter in avoided:
+			if letter in word:
+				passes_filter = False
+		if passes_filter:
+			print word
 
 def is_not_empty(word):
+	"Verifies user input is not empty."
 	if len(word) > 0:
 		return True
 	print "Your input must not be empty!"
 	return False
 
 def user_audit(word):
-	if are_letters(word):
-		if are_unique(word):
-			if is_not_empty(word):
-				return avoids(word)
+	# Removed the check for alphabetical input until I learn lists and tuples
+	# Removed the check for unique letters until I learn lists and tuples
+	if is_not_empty(word):
+		return avoids(word)
 	return user_input()
 
 def user_input():
-	avoided = raw_input('Please enter unique letters to avoid with no spaces:\n')
+	avoided = raw_input('Please enter unique letters to avoid:\n')
+	avoided = avoided.strip()	#Removes whitespace from beginning and end of input
+	avoided = avoided.replace(" ", "")	#Removes spaces from inside string
+	avoided = avoided.lower()	#Forces letters to become lowercase
 	user_audit(avoided)
 
 user_input()
